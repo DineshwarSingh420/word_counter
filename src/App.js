a4b07a4fb7210fdb2e922a60226e63463d5aacfe
DineshwarSingh420 <dineshwar420@gmail.com>
2023-09-28 13:38:56 +0530

    alert added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import { useState } from 'react';
 import './App.css';
 import About from './Components/About';
 import Textform from './Components/Textform';
-import Navbar from './Components/navbar.js'
+import Navbar from './Components/navbar.js';
+import Alert from './Components/Alert';
 
 function App() {
   const[Mode, setMode]=useState('light'); // whether dark mode is enable or not
+  const[alert, setAlert]=useState(null); // whether dark mode is enable or not
 
- const toggleMode=()=>{
+ const showAlert=(message,type)=>{  //passing message and its type
+setAlert({
+  msg:message,
+  type:type
+})
+ }
+ 
+  const toggleMode=()=>{
 if(Mode==='light'){
   setMode('dark')
   document.body.style.backgroundColor='black'
+  showAlert("Dark mode has been enabled","success ")
 }
 else{
   setMode('light')
   document.body.style.backgroundColor='white'
+  showAlert("Light mode has been enabled","success");
 
 }
   }
@@ -22,6 +33,7 @@ else{
   return (
     <>
 <Navbar title="Home_page" about="about_us" mode={Mode} toggleMode={toggleMode}/>
+<Alert alert="alert here"/>
 
 <div className="container">
 <Textform heading="Enter the Text Here"/>
